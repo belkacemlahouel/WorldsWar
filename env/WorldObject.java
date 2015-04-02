fc1dcf2d429588641730035de1f296a8c722cd9f
belkalah-mbp <belkacem.lahouel@gmail.com>
2015-04-02 16:23:18 +0200

    Maintenance operations.

--- env/WorldObject.java
+++ env/WorldObject.java
@@ -5,22 +5,6 @@ import math.MyPoint2D;
 public abstract class WorldObject {
 
 	public abstract String toString();
-	
-	public boolean isWall() {
-		return toString().contains("WALL");
-	}
-	
-	public boolean isPill() {
-		return toString().contains("PILL");
-	}
-	
-	public boolean isGhost() {
-		return toString().contains("GHOST");
-	}
-	
-	public boolean isPacman() {
-		return toString().contains("PACMAN");
-	}
 }
 
 interface Movable {
