0e889297dd91cebad3b514195e2d148ac1675e8a
belkalah-mbp <belkacem.lahouel@gmail.com>
2015-04-14 17:42:53 +0200

    Adding the body abstract definition with the mobile object (only movable).

---
+++ env2/api/AbstractBody.java
@@ -0,0 +1,7 @@
+package env2.api;
+
+public abstract class AbstractBody extends AbstractMobileWorldObject {
+
+	public abstract void buildNewFrustrum();
+	public abstract AbstractFrustrum getCurrentFrustrum();
+}
---
+++ env2/api/AbstractMobileWorldObject.java
@@ -0,0 +1,6 @@
+package env2.api;
+
+public abstract class AbstractMobileWorldObject extends AbstractWorldObject {
+
+	public abstract boolean move(AbstractCell arrival);
+}
