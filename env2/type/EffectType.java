6e9970dbfbf12b647a175e077898089d6a6c7ce7
belkalah-mbp <belkacem.lahouel@gmail.com>
2015-04-26 17:11:44 +0200

    Filling in these enumerations to begin implementation.

--- env2/type/EffectType.java
+++ env2/type/EffectType.java
@@ -4,4 +4,7 @@ public enum EffectType {
 	
 	// TODO FoodEffect ideas...
 	// In a first approach we may consider GOOD/BAD/NEUTRAL
+	GOOD,
+	BAD,
+	NEUTRAL;
 }
--- env2/type/WorldObjectType.java
+++ env2/type/WorldObjectType.java
@@ -3,4 +3,8 @@ package env2.type;
 public enum WorldObjectType {
 	
 	// TODO Add final classes names
+	TESTBODY,
+	SUGAR,
+	MEAT,
+	ROCK;
 }
