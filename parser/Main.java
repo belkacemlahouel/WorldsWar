a40f8baa3d4d545fa742b3b61d2bea62e611c538
MrJohnHenry <rdorier39@gmail.com>
2015-04-28 14:25:15 +0200

    Add a main class

---
+++ parser/Main.java
@@ -0,0 +1,19 @@
+package parser;
+
+import parser.EnvironmentParser;
+
+import java.io.IOException;
+
+class Main {
+	
+	public static void main(String [ ] args) throws IOException
+	{
+		EnvironmentParser datas = new EnvironmentParser("src/res/environments/environments.txt");
+		
+		System.out.println("Hello !");
+		
+		datas.parseEnvDatas();
+		
+		System.out.println(datas);
+	}
+}
