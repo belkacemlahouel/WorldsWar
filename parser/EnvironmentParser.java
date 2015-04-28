329e9276867111c3824268d5ba885a97d25e1a18
MrJohnHenry <rdorier39@gmail.com>
2015-04-28 14:51:45 +0200

    Add a main class containing a little main function.

--- parser/EnvironmentParser.java
+++ parser/EnvironmentParser.java
@@ -5,7 +5,17 @@ import java.nio.file.Paths;
 import java.nio.file.Path;
 import java.util.Scanner;
 
-
+/**
+ * The structure of the file parsed is the following
+ * globalSize=int*int	//the size of the global environment
+ * global=char[]		//char[] is an array of char, where e means earth (for a cell of earth), and w means water
+ * antSize=int*int		//size of the cave of the ants
+ * ant=char[]			//char[] is an array of char, where w means wall, and t means tunnel
+ * termiteSize=2*2		//size of the cave of the termites
+ * termite=char[]		//char[] is an array of char, where w means wall, and t means tunnel
+ * portals=[g,0,1
+ * ressources=
+ */
 class EnvironmentParser {
 	
 	private Path fFilePath;
@@ -59,6 +69,28 @@ class EnvironmentParser {
 	 */
 	public void parseLine(String line)
 	{
-		
+		//use a second Scanner to parse the content of each line 
+	    Scanner scanner = new Scanner(line);
+	    scanner.useDelimiter("=");
+	    if (scanner.hasNext()){
+	      //assumes the line has a certain structure
+	      String type = scanner.next();
+	      if(type.equals("name"))
+	      {
+	    	  String value = scanner.next();
+	    	  System.out.println("My name is "+ value );
+	      }
+	      else
+	      {
+	    	  String value = scanner.next();
+	    	  System.out.println("I am in the "+ value );
+	      }
+	      
+	    }
+	    else {
+	    	System.out.println("Invalid or empty line !");
+	    }
+	    
+	    scanner.close();
 	}
 }
