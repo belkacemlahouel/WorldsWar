4d477d3b38f82b12b4981e35cdee63393e8911bb
belkalah-mbp <belkacem.lahouel@gmail.com>
2015-04-30 20:20:05 +0200

    Changing semantics and recovering file.

--- env2/pheromones/Pheromone.java
+++ env2/pheromones/Pheromone.java
@@ -7,7 +7,7 @@ import env2.api.Morphable;
 import env2.env.Environment;
 import env2.type.WorldObjectType;
 
-public class Pheromones extends AbstractWorldObject implements Morphable{
+public class Pheromone extends AbstractWorldObject implements Morphable {
 
 	int count;
 	
@@ -15,7 +15,7 @@ public class Pheromones extends AbstractWorldObject implements Morphable{
 	AbstractEnvironment environment;
 	Environment globalEnv;
 	
-	public Pheromones(int count, MyPoint2D pos, AbstractEnvironment e){
+	public Pheromone(int count, MyPoint2D pos, AbstractEnvironment e) {
 		this.count = count;
 		position = pos;
 		this.environment = e;
@@ -24,48 +24,36 @@ public class Pheromones extends AbstractWorldObject implements Morphable{
 	public AbstractEnvironment getEnvironment() {
 		return environment;
 	}
+	
 	public MyPoint2D getPosition() {
 		return position;
 	}
 
-	@Override
 	public void tac() {
 		count --;
 	}
-	@Override
+	
 	public int getCount() {
 		return this.count;
 	}
-	@Override
+	
 	public boolean morph() {
 		System.out.println("delete");
 		return true;
 	}
 	
-	
-
-	@Override
 	public String toString() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	
-
-	@Override
 	public WorldObjectType getType() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public boolean isPerceivable() {
 		// TODO Auto-generated method stub
 		return false;
 	}
-
-	
-
-
-	
 }
