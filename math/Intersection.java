ab2bb1018aa7b4b16add0c11ec7548d2c5ecaae0
belkalah-mbp <belkacem.lahouel@gmail.com>
2015-05-01 08:53:49 +0200

    Adding a class to keep computation of Intersections.

---
+++ math/Intersection.java
@@ -0,0 +1,12 @@
+package math;
+
+public class Intersection {
+	
+	public final float time;
+	public final MyPoint2D position;
+	
+	public Intersection(float time, int x, int y) {
+		this.time = time;
+		position = new MyPoint2D(x, y);
+	}
+}
