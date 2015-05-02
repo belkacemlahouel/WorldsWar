f5ed4b7e98bbaf6689ab0d153df77e5cbbefa986
belkalah-mbp <belkacem.lahouel@gmail.com>
2015-05-02 23:34:48 +0200

    New action implemented: pick.
    (need to be called by something else, of course.)

---
+++ env2/action/PickAction.java
@@ -0,0 +1,28 @@
+package env2.action;
+
+import env2.api.AbstractAction;
+import env2.api.AbstractResource;
+import env2.api.InterfaceGatherer;
+
+/**
+ * We assume only a body can pick a resource.
+ * Therefore, this action is available only for those types.
+ * @author belka
+ *
+ */
+
+public class PickAction extends AbstractAction {
+
+	private InterfaceGatherer picker;
+	private AbstractResource picked;
+	private int qty;
+	
+	public PickAction(InterfaceGatherer body, AbstractResource resource, int qty) {
+		picker = body;
+		picked = resource;
+	}
+
+	public void doAction() {
+		picker.take(picked, qty);
+	}
+}
