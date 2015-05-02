6c1daed4f6d119aae37479d7f1f75f88c1564791
belkalah-mbp <belkacem.lahouel@gmail.com>
2015-05-02 23:33:35 +0200

    Small modifications.
    Types: separation of pick/put.
    InfluenceGatherer: works with AResources only now.

--- env2/api/InterfaceGatherer.java
+++ env2/api/InterfaceGatherer.java
@@ -2,7 +2,8 @@ package env2.api;
 
 public interface InterfaceGatherer {
 	
-	public abstract void take(AbstractWorldObject o, int qty);
+	public abstract void take(AbstractResource o, int qty);
+	// public abstract void take(AbstractWorldObject o, int qty);
 	public abstract int getStdTakeQty();
 	public abstract int getRndTakeQty();
 	public abstract int getTotTakeQty();
--- env2/type/InfluenceType.java
+++ env2/type/InfluenceType.java
@@ -2,7 +2,8 @@ package env2.type;
 
 public enum InfluenceType {
 	MOTION,
-	ATTACK_CURE,
-	PICK_PUT,
+	ATTACK_CURE, // Because it's basically the same action, the "damage" is just positive/negative
+	PICK,
+	PUT,
 	EAT;
 }
