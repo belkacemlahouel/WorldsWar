8f228ef97b082985890690fb4f08a0729746fc40
MrJohnHenry <rdorier39@gmail.com>
2015-05-09 16:23:56 +0200

    New tests for the bodies. There is an error due to a wrong import in GUI.java.

--- parser/Main.java
+++ parser/Main.java
@@ -1,14 +1,18 @@
 package parser;
 
 import parser.EnvironmentParser;
-import env.Direction;
-import env.body.AntBody;
-import env.environment.Environment;
-import env.environment.Ground;
+import env2.type.Direction;
+import env2.body.TestBody;
+import env2.env.Environment;
+import env2.env.Ground;
 import gui.AgentBodyGUI;
 import gui.GUI;
+import math.MyPoint2D;
+import env2.api.AbstractEnvironment;
 
 import java.io.IOException;
+import java.util.Date;
+
 
 class Main {
 	
@@ -25,8 +29,10 @@ class Main {
 		//Ground env = new Ground(10, 10);
 		GUI gui = new GUI(env);
 		
-		/*AntBody antTest = new AntBody(env,Direction.NORTH);
-		AgentBodyGUI antTestGui = new AgentBodyGUI("Ant",gui,antTest);*/
+		float time = new Date().getTime();
+		MyPoint2D newPos = new MyPoint2D(1,1);
+		TestBody antTest = new TestBody(env,Direction.NORTH, newPos, time);
+		AgentBodyGUI antTestGui = new AgentBodyGUI("Ant",gui,antTest);
 		
 	}
 }
