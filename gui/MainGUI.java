cfa2472c4dab60880fb07b2258b8e6120c2596e2
Bourgeois <bourgeois.maxime@gmail.com>
2015-05-09 20:25:15 +0200

    Fixing GUI with env2 (correct one)

--- gui/MainGUI.java
+++ gui/MainGUI.java
@@ -1,13 +1,12 @@
 package gui;
 
+import env2.api.AbstractEnvironment;
 import env2.env.Ground;
 
 public class MainGUI {
 
 	public static void main(String[] args) {		
-		Ground env = new Ground(50, 50);
-		// GUI window = new GUI(env);
-		
-		// TODO Check all the code regarding GUI to work with env2/
+		AbstractEnvironment env = new Ground(50, 50);
+		GUI window = new GUI(env);
 	}
 }
