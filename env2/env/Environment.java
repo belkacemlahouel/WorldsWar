4410527a33079f5490c750dddfd4277f8d808a02
belkalah-mbp <belkacem.lahouel@gmail.com>
2015-05-15 10:51:17 +0200

    Modifying this class to be more POO-suitable.

--- env2/env/Environment.java
+++ env2/env/Environment.java
@@ -2,7 +2,25 @@ package env2.env;
 
 import env2.api.AbstractEnvironment;
 
+/**
+ * This class may seem useless,
+ * but I am thinking about adding
+ * other methods inside to make it useful
+ * *** ENHANCEMENT ***
+ * 
+ * @author belka
+ *
+ */
+
 public class Environment {
 
-	public AbstractEnvironment[] grounds;
+	private AbstractEnvironment[] grounds;
+	
+	public Environment(int size) {
+		grounds = new AbstractEnvironment[size];
+	}
+	
+	public AbstractEnvironment get(int i) {
+		return grounds[i];
+	}
 }
