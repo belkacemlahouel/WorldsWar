033132680a55d7e011e1d6f59d49344882145f37
belkalah-mbp <belkacem.lahouel@gmail.com>
2015-05-15 23:59:56 +0200

    Adding resources class (still need implementation).

---
+++ env2/resources/Poison.java
@@ -0,0 +1,38 @@
+package env2.resources;
+
+import env2.api.AbstractResource;
+import env2.type.WorldObjectType;
+
+public class Poison extends AbstractResource {
+
+	@Override
+	public int getQuantity() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public AbstractResource pick(int quantity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void add(int quantity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public WorldObjectType getType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isPerceivable() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
