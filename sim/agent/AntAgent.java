6f0970c94fb30617a1320cb3db4fed737886951d
belkalah-mbp <belkacem.lahouel@gmail.com>
2015-05-15 11:11:19 +0200

    (#20) Started. The body of this method still needs to be checked...
    TODO: Add specializations for decision making as well (Agent part).
    @belkacemlahouel

---
+++ sim/agent/AntAgent.java
@@ -0,0 +1,18 @@
+package sim.agent;
+
+import env2.body.AntBody;
+import sim.AbstractAgent;
+
+public class AntAgent extends AbstractAgent {
+
+	public AntAgent(AntBody b, int ID) {
+		super(b, ID);
+	}
+
+	/**
+	 * Here is the whole decision process to implement
+	 */
+	public void live() {
+		// TODO
+	}
+}
