a46031c28b39ffcae6e7f1e373f85741e15a05fa
belkalah-mbp <belkacem.lahouel@gmail.com>
2015-05-15 11:55:58 +0200

    SpiderAgent methods to be implemented.

---
+++ sim/agent/spideragent/SpiderAgent.java
@@ -0,0 +1,19 @@
+package sim.agent.spideragent;
+
+import env2.api.AbstractBody;
+import sim.AbstractAgent;
+
+public class SpiderAgent extends AbstractAgent {
+
+	public SpiderAgent(AbstractBody b, int ID) {
+		super(b, ID);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void live() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
