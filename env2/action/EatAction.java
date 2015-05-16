1f074c67c6adbf85b0821134e0c27e96aae735d1
belkalah-mbp <belkacem.lahouel@gmail.com>
2015-05-16 18:30:25 +0200

    Adding EatAction.

--- env2/action/EatAction.java
+++ env2/action/EatAction.java
@@ -1,6 +1,27 @@
 package env2.action;
 
+import java.util.Collection;
+
+import env2.api.AbstractBody;
+import env2.api.AbstractResource;
+
 public class EatAction {
 	
-	// TODO
+	protected AbstractBody eater;
+	protected int qty;
+	protected AbstractResource resource;
+	protected Collection<AbstractResource> container;
+	
+	public EatAction(AbstractBody eater, Collection<AbstractResource> container, AbstractResource resource, int qty) {
+		this.qty = qty;
+		this.eater = eater;
+		this.resource = resource;
+		this.container = container;
+	}
+
+	public void doAction() {
+		eater.eat(resource, qty);
+		if (resource.getQuantity() <= 0)
+			container.remove(resource);
+	}
 }
