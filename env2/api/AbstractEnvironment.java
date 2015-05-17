4ebdf90bc43dcb7ad43f5661ebe4c0b46f3605c6
belkalah-mbp <belkacem.lahouel@gmail.com>
2015-05-17 20:09:14 +0200

    Adding methods to put/take resources (Gatherer) and another implementation to getPosition in Environment

--- env2/api/AbstractEnvironment.java
+++ env2/api/AbstractEnvironment.java
@@ -1,8 +1,14 @@
 package env2.api;
 
+import math.MyPoint2D;
+
 public abstract class AbstractEnvironment {
 
 	public abstract AbstractCell getCell(int x, int y);
 	public abstract int getWidth();
 	public abstract int getHeight();
+	
+	public AbstractCell getCell(MyPoint2D position) {
+		return getCell(position.getX(), position.getY());
+	}
 }
--- env2/body/antbody/AntGathererBody.java
+++ env2/body/antbody/AntGathererBody.java
@@ -1,9 +1,12 @@
 package env2.body.antbody;
 
+import java.util.Collection;
+
 import math.MyMath;
 import math.MyPoint2D;
 import env2.api.AbstractEnvironment;
 import env2.api.AbstractResource;
+import env2.api.AbstractWorldObject;
 import env2.api.InterfaceGatherer;
 import env2.type.Direction;
 
@@ -21,19 +24,36 @@ public final class AntGathererBody extends AntBody implements InterfaceGatherer 
 
 	/*
 	 * We assume the quantity provided in this method comes from the InfluenceSolver
-	 * Hence, we should never go pass our capacity
+	 * Hence, we should never go pass our capacity...
 	 * 
 	 * (non-Javadoc)
 	 * @see env2.api.InterfaceGatherer#take(env2.api.AbstractResource, int)
 	 */
+	private AbstractResource getResourceOfSameType(AbstractResource o, Collection<? extends AbstractWorldObject> container) {
+		for (AbstractWorldObject res : container) {
+			if (res.getType() == o.getType()) {
+				return ((AbstractResource) res);
+			}
+		}
+		
+		return null;
+	}
+	
 	public void take(AbstractResource o, int qty) {
-		// TODO Auto-generated method stub
+		AbstractResource res = getResourceOfSameType(o, getCargo());
+		if (res == null)
+			getCargo().add(o);
+		
+		res.add(qty);
 	}
 
 	@Override
 	public void put(AbstractResource o, int qty) {
-		// TODO Auto-generated method stub
+		AbstractResource res = getResourceOfSameType(o, getEnvironment().getCell(getPosition()).getObjects());
+		if (res == null)
+			getEnvironment().getCell(getPosition()).getObjects().add(o);
 		
+		res.add(qty);
 	}
 
 	@Override
