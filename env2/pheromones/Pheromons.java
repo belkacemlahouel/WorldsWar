6475ebdc9cbc94307c83a979b732ccf8b0f8f336
MrJohnHenry <rdorier39@gmail.com>
2015-05-21 18:53:51 +0200

    Changes in the structure of pheromones.

--- env2/pheromones/Pheromons.java
+++ env2/pheromones/Pheromons.java
@@ -6,10 +6,12 @@ import java.util.List;
 
 import env2.api.AbstractBody;
 import env2.api.AbstractEnvironment;
+import env2.api.AbstractWorldObject;
+import env2.type.Time;
+import env2.type.WorldObjectType;
 import math.MyPoint2D;
 
-
-//TODO add another level where the pheromon is less "fellable". 
+ 
 /**
  * Representation of an object pheromon.
  * 
@@ -19,25 +21,29 @@ import math.MyPoint2D;
  * X X X
  * Where O is the position of the pheromon.
  */
-class Pheromons {
-	private PheromonsType type;
+public class Pheromons extends AbstractWorldObject{
+	private WorldObjectType type;
 	private int remainingTime;
 	private AbstractEnvironment env;
 	//tribeId is the id of the tribe which can detect the pheromon.
 	private int tribeId;
 	private MyPoint2D pos;
-	//surface contains the surface where the pheromon can be detect by an agent and the "fellable" characteristic.
-	private HashMap<MyPoint2D, Integer> surface = new HashMap<MyPoint2D, Integer>(8);
+	private float power;
 	
-	Pheromons(PheromonsType pType, AbstractEnvironment environment, int id, MyPoint2D position)
+	Pheromons(WorldObjectType pType, AbstractEnvironment environment, int id, MyPoint2D position, float pow)
 	{
 		type = pType;
 		remainingTime = getMaxLapse();
 		env = environment;
 		tribeId = id;
 		pos = position;
+		power = pow;
 		
-		
+		if(power == 2.0)
+		{
+			Time time = new Time();
+			buildSurface(pos, time);
+		}
 	}
 	
 	/**
@@ -64,7 +70,7 @@ class Pheromons {
 	}
 
 	/**
-	 * Delete a pheromons.
+	 * Delete a pheromon.
 	 */
 	private void delete()
 	{
@@ -77,42 +83,106 @@ class Pheromons {
 	 * Create the surface detected by an agent
 	 * @param position, the position of the pheromon.
 	 */
-	private void buildSurface(MyPoint2D position)
+	private void buildSurface(MyPoint2D position, Time time)
 	{
 		
 				MyPoint2D newPos = position;
 				newPos.add(-1,1);
-				surface.put(newPos,1);
+				if(testExistence(newPos))
+				{
+					new Pheromons(type, env, tribeId, newPos, powerRegardToTimeAfterCreation(1.0, time));
+				}
 				
 				newPos = position;
 				newPos.add(0,1);
-				surface.put(newPos,1);
+				if(testExistence(newPos))
+				{
+					new Pheromons(type, env, tribeId, newPos, powerRegardToTimeAfterCreation(1.0, time));
+				}
 				
 				newPos = position;
 				newPos.add(1,1);
-				surface.put(newPos,1);
+				if(testExistence(newPos))
+				{
+					new Pheromons(type, env, tribeId, newPos, powerRegardToTimeAfterCreation(1.0, time));
+				}
 				
 				newPos = position;
 				newPos.add(-1,0);
-				surface.put(newPos,1);
-				
-				surface.put(position,2);
+				if(testExistence(newPos))
+				{
+					new Pheromons(type, env, tribeId, newPos, powerRegardToTimeAfterCreation(1.0, time));
+				}
 				
 				newPos = position;
 				newPos.add(1,0);
-				surface.put(newPos,1);
+				if(testExistence(newPos))
+				{
+					new Pheromons(type, env, tribeId, newPos, powerRegardToTimeAfterCreation(1.0, time));
+				}
 				
 				newPos = position;
 				newPos.add(-1,-1);
-				surface.put(newPos,1);
+				if(testExistence(newPos))
+				{
+					new Pheromons(type, env, tribeId, newPos, powerRegardToTimeAfterCreation(1.0, time));
+				}
 				
 				newPos = position;
 				newPos.add(0,-1);
-				surface.put(newPos,1);
+				if(testExistence(newPos))
+				{
+					new Pheromons(type, env, tribeId, newPos, powerRegardToTimeAfterCreation(1.0, time));
+				}
 				
 				newPos = position;
 				newPos.add(1,-1);
-				surface.put(newPos,1);
+				if(testExistence(newPos))
+				{
+					new Pheromons(type, env, tribeId, newPos, powerRegardToTimeAfterCreation(1.0, time));
+				}
+	}
+	
+	/**
+	 * Dicrease the power of a pheromon in function of the time passed since the demand of creation.
+	 * @param d
+	 * @return the power dicreased.
+	 */
+	private float powerRegardToTimeAfterCreation(double d, Time time)
+	{
+		return (float) d;
+	}
+
+	/**
+	 * Test if a position exist in the environment.
+	 * @param pos the position to test.
+	 * @return the result of the test.
+	 */
+	private boolean testExistence(MyPoint2D pos)
+	{
+		int width = env.getWidth();
+		int height = env.getHeight();
+		
+		if(pos.getX()<=width && pos.getY()<=height)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	/**
+	 * Return type of the pheromon.
+	 */
+	public WorldObjectType getType() {
+		return type;
+	}
+
+	@Override
+	public boolean isPerceivable() {
+		return true;
 	}
 	
 	/**
@@ -120,7 +190,7 @@ class Pheromons {
 	 * @param body, the agent's body that can potentialy feel the pheromon.
 	 * @return true, if the body can detect the pheromon.
 	 */
-	public boolean isDetected(AbstractBody body)
+	/*public boolean isDetected(AbstractBody body)
 	{
 		MyPoint2D pos = body.getPosition();
 		int id = body.getTribeID();
@@ -135,5 +205,5 @@ class Pheromons {
 			//check if the body is in the area to feel the pheromon.
 			return this.surface.containsKey(pos);
 		}
-	}
+	}*/
 }
