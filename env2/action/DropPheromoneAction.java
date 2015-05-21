78b84ef20f7812ff82b663bcde6c05354484afbd
belkacem-tosh-win8 <belkacem.lahouel@gmail.com>
2015-05-21 21:31:58 +0200

    Updating DropPheromone due to new Action.

--- env2/action/DropPheromoneAction.java
+++ env2/action/DropPheromoneAction.java
@@ -1,24 +1,25 @@
 package env2.action;
 
+import java.util.LinkedList;
 import java.util.NoSuchElementException;
 
+import math.MyMath;
 import math.MyPoint2D;
 import env2.api.AbstractAction;
-import env2.api.AbstractCell;
 import env2.api.AbstractEnvironment;
 import env2.pheromones.Pheromone;
 import env2.type.WorldObjectType;
 
 public class DropPheromoneAction extends AbstractAction {
 
-	private final float CREATION_TIME;
+	private final int CREATION_TIME;
 	private final int TRIBE_ID;
 	private final WorldObjectType TYPE;
 	private final AbstractEnvironment ENV;
 	private final MyPoint2D POS;
 	private final float POW;
 	
-	public DropPheromoneAction(WorldObjectType type, AbstractEnvironment e, MyPoint2D position, int tribe_id, float power, float time) {
+	public DropPheromoneAction(WorldObjectType type, AbstractEnvironment e, MyPoint2D position, int tribe_id, float power, int time) {
 		if (WorldObjectType.isPheromone(type)) {
 			CREATION_TIME = time;
 			TRIBE_ID = tribe_id;
@@ -37,18 +38,20 @@ public class DropPheromoneAction extends AbstractAction {
 	}
 	
 	private void dispatch() {
-		/* top cells */
-		ENV.getCell(POS.addNew(-1, 1)).addObject(new Pheromone(TYPE, ENV, TRIBE_ID, POS.addNew(-1, 1), POW*Pheromone.SPREADING_RATIO, CREATION_TIME));
-		ENV.getCell(POS.addNew(0, 1)).addObject(new Pheromone(TYPE, ENV, TRIBE_ID, POS.addNew(0, 1), POW*Pheromone.SPREADING_RATIO, CREATION_TIME));
-		ENV.getCell(POS.addNew(1, 1)).addObject(new Pheromone(TYPE, ENV, TRIBE_ID, POS.addNew(1, 1), POW*Pheromone.SPREADING_RATIO, CREATION_TIME));
+		LinkedList<MyPoint2D> all_points = new LinkedList<>();
 		
-		/* middle cells */
-		ENV.getCell(POS.addNew(-1, 0)).addObject(new Pheromone(TYPE, ENV, TRIBE_ID, POS.addNew(-1, 0), POW*Pheromone.SPREADING_RATIO, CREATION_TIME));
-		ENV.getCell(POS.addNew(1, 0)).addObject(new Pheromone(TYPE, ENV, TRIBE_ID, POS.addNew(1, 0), POW*Pheromone.SPREADING_RATIO, CREATION_TIME));
+		all_points.add(POS.addNew(-1, 1));
+		all_points.add(POS.addNew(0, 1));
+		all_points.add(POS.addNew(1, 1));
+		all_points.add(POS.addNew(-1, 0));
+		all_points.add(POS.addNew(1, 0));
+		all_points.add(POS.addNew(-1, -1));
+		all_points.add(POS.addNew(0, -1));
+		all_points.add(POS.addNew(1, -1));
 		
-		/* bottom cells */
-		ENV.getCell(POS.addNew(-1, -1)).addObject(new Pheromone(TYPE, ENV, TRIBE_ID, POS.addNew(-1, -1), POW*Pheromone.SPREADING_RATIO, CREATION_TIME));
-		ENV.getCell(POS.addNew(0, -1)).addObject(new Pheromone(TYPE, ENV, TRIBE_ID, POS.addNew(0, -1), POW*Pheromone.SPREADING_RATIO, CREATION_TIME));
-		ENV.getCell(POS.addNew(1, -1)).addObject(new Pheromone(TYPE, ENV, TRIBE_ID, POS.addNew(1, -1), POW*Pheromone.SPREADING_RATIO, CREATION_TIME));
+		for (MyPoint2D tmp : all_points) {
+			if (MyMath.isIn(tmp.getX(), tmp.getY(), ENV.getWidth(), ENV.getHeight()))
+				ENV.getCell(tmp).addObject(new Pheromone(TYPE, ENV, TRIBE_ID, tmp, POW*Pheromone.SPREADING_RATIO, CREATION_TIME));
+		}
 	}
 }
