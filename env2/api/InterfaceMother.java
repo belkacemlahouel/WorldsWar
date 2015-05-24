1e7801803b4cf75e85db96b203953afc6d9c9441
belkalah-mbp <belkacem.lahouel@gmail.com>
2015-05-24 17:51:34 +0200

    Issue #43: how to connect new agents to their bodies... How to add them in the simulation loop?

--- env2/api/InterfaceMother.java
+++ env2/api/InterfaceMother.java
@@ -3,4 +3,6 @@ package env2.api;
 public interface InterfaceMother {
 
 	public abstract void addBaby();
+	// How to connect an agent to the newly created body?
+	// AND TODO we have to find a way to add this agent to the simulation loop.
 }
