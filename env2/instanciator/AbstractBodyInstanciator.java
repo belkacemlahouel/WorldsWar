e697028887d463b1ec1f508c512f1d2dd94ba32a
belkalah-mbp <belkacem.lahouel@gmail.com>
2015-05-25 15:09:44 +0200

    Adding Body instanciator...

---
+++ env2/instanciator/AbstractBodyInstanciator.java
@@ -0,0 +1,8 @@
+package env2.instanciator;
+
+import env2.api.AbstractBody;
+
+public abstract class AbstractBodyInstanciator extends AbstractInstanciator {
+	
+	public abstract AbstractBody getNew();
+}
--- env2/instanciator/AbstractResourceInstanciator.java
+++ env2/instanciator/AbstractResourceInstanciator.java
@@ -2,7 +2,7 @@ package env2.instanciator;
 
 import env2.api.AbstractResource;
 
-public abstract class AbstractResourceInstanciator {
+public abstract class AbstractResourceInstanciator extends AbstractInstanciator {
 
 	public abstract AbstractResource getNew();
 }
