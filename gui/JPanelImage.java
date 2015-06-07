babad21d410f6f68f8216c181b38f5999982ebdb
Bourgeois <bourgeois.maxime@gmail.com>
2015-06-07 21:20:44 +0200

    Resources added + various small changes

---
+++ gui/JPanelImage.java
@@ -0,0 +1,24 @@
+package gui;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JPanel;
+
+public class JPanelImage extends JPanel {
+	private static final long serialVersionUID = 4641683667519659647L;
+	
+	private BufferedImage img = null;
+    
+    public JPanelImage(BufferedImage image) {
+		this.img = image;
+	}
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        
+        if(this.img != null){
+        	g.drawImage(img, 0, 0, null);
+        }
+    }
+}
